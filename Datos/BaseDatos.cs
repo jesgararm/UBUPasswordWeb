1ceeaa77dcd781b98c03ba4af9d35fa6bacd989d
Jesús GA <94868006+jesgararm@users.noreply.github.com>
2022-11-07 23:39:35 +0100

    Revert "Se implementa directorio de Usuarios.csv"
    This reverts commit ab0e0f26d33e012307c39534cd06b677b57a1650.

--- Datos/BaseDatos.cs
+++ Datos/BaseDatos.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using ClasesLib;
 /// <summary>
@@ -33,7 +32,7 @@ namespace Datos
 
 			Usuario admin = new Usuario("admin@gmail.com", "admin", "admin", "admin",true);
 			InsertarUsuario(admin);
-			string ubicacion = System.IO.Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, "..\\Datos\\Usuarios.csv");
+			string ubicacion = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\Datos\\Usuarios.csv");
             System.IO.StreamReader archivo = new System.IO.StreamReader(ubicacion);
 			char separador = ';';
 			string linea;
