c09a04e685965bdc01e306323cebb12b7b71cb95
Jesús GA <94868006+jesgararm@users.noreply.github.com>
2022-11-13 13:18:36 +0100

    Se añade Studio Project

---
+++ KR Exported Studio Project/Test Suites/wwwTests.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>wwwTests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9bc8e10c-90d4-4b4a-bd17-b20f91f538f1</testSuiteGuid>
+   <testCaseLink>
+      <guid>e7534372-2c74-400b-9ba3-51a5f154ed70</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/wwwTests/CP002</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>784407e1-a840-4500-a812-40a87326f97e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/wwwTests/CP003</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>09c00eb4-c41c-48a2-baca-287abba42ee9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/wwwTests/CP005</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>1880aa2a-37a0-4efd-964c-a698e7938f52</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/wwwTests/CP004</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>a60f861b-ae72-46ac-8ee9-5298e4227735</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/wwwTests/CP006</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>b953920c-ce0c-40a8-81d5-d72bdf3601dd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/wwwTests/CP007</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>66d02b25-e156-4dda-bc1b-da002751a5aa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/wwwTests/CP008</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
